011616afc6127e8dd23ccce19b63317c15a55ea8
Muhammad Abdul Hanan <141907778+AbdulHanan164@users.noreply.github.com>
2025-12-16 00:09:44 +0500

    Add assertion to check model file existence

--- tests/test_train.py
+++ tests/test_train.py
@@ -4,3 +4,4 @@ def test_model_exists():
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     model_path = os.path.join(project_root, "models", "model.pkl")
     assert os.path.exists(model_path)
+
